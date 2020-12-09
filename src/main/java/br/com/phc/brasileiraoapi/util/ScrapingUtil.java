3353daff8d50ed2ef4aabfc75c773faa10f0dc6b
brunohenrique19 <brunohgloria7@gmail.com>
2020-12-08 22:12:44 -0300

    recuperando status e tempo da partida

--- src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
@@ -17,7 +17,7 @@ public class ScrapingUtil {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String url = BASE_URL_GOOGLE + "palmeiras+x+corinthians+08/08/2020" + COMPLEMENTO_URL_GOOGLE;
+		String url = BASE_URL_GOOGLE + "bastia+x+cholet" + COMPLEMENTO_URL_GOOGLE;
 		
 		ScrapingUtil scraping = new ScrapingUtil();
 		scraping.obtemInformacoesPartida(url);
@@ -35,11 +35,61 @@ public class ScrapingUtil {
 			String title = document.title();
 			LOGGER.info("Titulo da pagina: {}", title);
 			
+			StatusPartida statusPartida = obtemStatusPartida(document);
+			LOGGER.info("Status partida: {}", statusPartida.toString());
+			String tempoPartida = obtemTempoPartida(document);
+			LOGGER.info("Tempo partida: {}", tempoPartida);
+			
 		} catch (IOException e) {
 			LOGGER.error("ERRO AO CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
 		}
 			
 		return partida;
+	}
+	
+	public StatusPartida obtemStatusPartida(Document document) {
+		StatusPartida statusPartida = StatusPartida.PARTIDA_NAO_INICIADA;
+		
+		boolean isTempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		
+		if(!isTempoPartida) {
+			String tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+			statusPartida = StatusPartida.PARTIDA_EM_ANDAMENTO;
+			if(tempoPartida.contains("Pênaltis")) {
+				statusPartida = StatusPartida.PARTIDA_PENALTIS;
+			}
+		}
+		
+		isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+		if(!isTempoPartida) {
+			statusPartida = StatusPartida.PARTIDA_ENCERRADA;
+		}
+		
+		return statusPartida;
+		
+	}
+	
+	public String obtemTempoPartida(Document document) {
+		String tempoPartida = null;
+		
+		boolean isTempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
+		if(!isTempoPartida) {
+			tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
+		}
+		isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+		if(!isTempoPartida) {
+			tempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").first().text();
+		}
+		return corrigeTempoPartida(tempoPartida);
+	}
+	
+	public String corrigeTempoPartida(String tempo) {
+		
+		if(tempo.contains("'")) {
+			return tempo.replace(" ", "").replace("'", " min");
+		} else {
+			return tempo;
+		}
 	}
 
 }
